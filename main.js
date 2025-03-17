ff91ed27bf7c5ac16fcfbf1fccd00e18d8ebf50a
joaovitorpapalardo <papalardojo@gmail.com>
2025-03-17 15:05:58 -0300

    add JS

---
+++ main.js
@@ -0,0 +1,43 @@
+window.revelar = ScrollReveal({reset:true})
+
+//principal
+revelar.reveal('.start-your-workday, .h4-textos-informacoes, .linha-safety', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+
+})
+
+revelar.reveal('.texto-principal, .texto-h2-started, .linha-driveon', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'right'
+
+})
+
+revelar.reveal('.btn-discover, .img-secao-principal, .img-informacoes-started, .link-safety', {
+    duration: 2300,
+    distance: '90px',
+    origin: 'bottom',
+})
+
+revelar.reveal('.linha-started, .p-textos-informacoes, .img-secao-driveon, .h4-informacoes-safety, #application, .register', {
+    duration: 2300,
+    distance: '90px',
+    origin: 'left'
+
+})
+
+revelar.reveal('.informacoes-driveon, .p-informacoes-safety', {
+    duration: 2300,
+    distance: '90px',
+    origin: 'right'
+
+})
+
+revelar.reveal('.img-secao-safety', {
+    duration: 2300,
+    distance: '90px',
+    origin: 'top'
+
+})
